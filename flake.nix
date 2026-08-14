01273fc38880771dce8f478e623af1159243bc9d
tonyhawklover <tonyhawklover@proton.me>
2026-08-14 14:43:38 -0700

    use nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "gas-clone dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.nodejs_24 ];
+
+          shellHook = ''
+            echo "node $(node --version)"
+          '';
+        };
+      });
+}
